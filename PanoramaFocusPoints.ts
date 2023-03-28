7183dad8362c57cf4905d69fa220d79ba1d7666e
Maëlle <maelle@upian.com>
2023-03-28 12:30:48 +0200

    feat(): 360 panorama + focus points

--- PanoramaFocusPoints.ts
+++ PanoramaFocusPoints.ts
@@ -1,21 +1,22 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 export default class PanoramaFocusPoints {
     private canvas: HTMLCanvasElement;
     private renderer: THREE.WebGLRenderer;
     private camera: THREE.OrthographicCamera;
+    private controls: OrbitControls;
     private scene: THREE.Scene;
-    private plane: THREE.Mesh;
-    private texAspect: number;
+    private sphere: THREE.Mesh;
+    private rayCaster: THREE.Raycaster;
     private imgTex: THREE.Texture;
     private targetColorRGB: Array<[number, number, number]>;
-    private DOMElements: Array<HTMLElement>;
+    private DOMElements: Array<{ position: THREE.Vector3, element: HTMLElement}>;
     private nbTargets: number;
     private step: number;
     private posPoints: Array<object>;
   
-    constructor(canvas: HTMLCanvasElement, imgURL: string, targetColorRGB: Array<[number, number, number]>, DOMElements: Array<HTMLElement> ) {
-      this.targetColorRGB = targetColorRGB
+    constructor(canvas: HTMLCanvasElement, imgURL: string, DOMElements: Array<{ position: THREE.Vector3, element: HTMLElement}> ) {
       this.nbTargets = DOMElements.length
       this.DOMElements = DOMElements
       
@@ -27,103 +28,76 @@ export default class PanoramaFocusPoints {
       this.renderer.setSize(window.innerWidth, window.innerHeight)
       this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   
-      this.camera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, -1000, 1000)
-      this.camera.position.set(0, 0, 1)
+      this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 )
+      this.camera.target = new THREE.Vector3( 0, 0, 0 );
+      this.camera.position.set( 0, 0, 10 );
   
       this.scene = new THREE.Scene()
   
       this.imgTex = new THREE.TextureLoader().load(imgURL, () => {
-        this.texAspect = this.imgTex.image.naturalWidth / this.imgTex.image.naturalHeight
-  
-        this.plane = new THREE.Mesh(
-          new THREE.PlaneGeometry(1, 1, 32, 32),
+        this.sphere = new THREE.Mesh(
+          new THREE.SphereGeometry( 500, 60, 40 ).scale( - 1, 1, 1 ),
           new THREE.MeshBasicMaterial({ map: this.imgTex })
         );
-        this.fitPlaneToScreen()
-        this.scene.add(this.camera, this.plane)
-        this.updatePosPointer()
+        this.scene.add(this.camera, this.sphere)
       })
   
       window.addEventListener('resize', () => {
         this.renderer.setSize(window.innerWidth, window.innerHeight)
         this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-        this.fitPlaneToScreen()
-        this.updatePosPointer()
-      })
-    }
-  
-    private isAroundValue(val, target, gap) {
-      if ((val + gap) > target && (val - gap) < target) {
-        return true
-      } else {
-        return false
-      }
-    }
-  
-    private fitPlaneToScreen() {
-      let scale
-      const viewAspect = window.innerWidth / window.innerHeight
-  
-      if (this.texAspect > viewAspect) {
-        scale = [this.texAspect / viewAspect, 1]
-      } else {
-        scale = [1, viewAspect / this.texAspect]
-      }
-      this.plane.scale.set(scale[0], scale[1], 1)
-    }
-  
-    private updatePosPointer() {
-      this.DOMElements.forEach(domPointers => {
-        domPointers.style.display = 'none'
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
       })
 
-      this.step = 0
-      this.posPoints = []
+      this.controls = new OrbitControls( this.camera, this.canvas )
+      this.controls.enableZoom = false
+      
+      this.tick()
+
+
+      // DEBUG
+      this.rayCaster = new THREE.Raycaster()
+      // window.addEventListener('click', (e) => this.pointPositioning(e))
+    }
+
+    private tick(){
+      this.DOMElements.forEach((el) => this.updatePosPoint(el))
+      
+      this.controls.update();
       this.renderer.render(this.scene, this.camera)
-      const img = new Image();
-      img.src = this.renderer.domElement.toDataURL("image/png");
-  
-      img.onload = () => {
-        const canvas = document.createElement('canvas');
-        canvas.width = img.width;
-        canvas.height = img.height;
-  
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0);
-  
-        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const data = imageData.data;
-  
-        for (let i = 0; i < data.length; i += 4) {
-          const r = data[i];
-          const g = data[i + 1];
-          const b = data[i + 2];
-          if (this.isAroundValue(r, this.targetColorRGB[0], 30) && this.isAroundValue(g, this.targetColorRGB[1], 30) && this.isAroundValue(b, this.targetColorRGB[2], 30)) {
-            let x = (i / 4) % canvas.width;
-            let y = Math.floor(i / 4 / canvas.width);
-  
-            x = x * (window.innerWidth / img.width)
-            y = y * (window.innerHeight / img.height)
-  
-            if(this.posPoints.length === 0 || !this.isAroundValue(this.posPoints[this.posPoints.length-1].x, x, 15) || !this.isAroundValue(this.posPoints[this.posPoints.length-1].y, y, 15)){
-              this.posPoints.push({ x: x, y: y})
-
-              this.DOMElements[this.step].style.display = 'block'
-              this.DOMElements[this.step].style.left = `${x - 10}px`
-              this.DOMElements[this.step].style.top = `${y - 10}px`
-  
-              this.step++
-              if (this.step === this.nbTargets) {
-                console.log('Pixels positions', this.getPosPoints())
-                break;
-              }
-            }
-          }
-        }
-      };
+      window.requestAnimationFrame(() => this.tick())
     }
-  
+
+    private updatePosPoint(point){
+      const screenPosition = point.position.clone()
+      screenPosition.project(this.camera)
+
+      const translateX = window.innerWidth/2 + (screenPosition.x * window.innerWidth) * 0.5
+      const translateY = window.innerHeight/2 - (screenPosition.y * window.innerHeight) * 0.5
+
+      if (screenPosition.z < 1) {
+        point.element.style.display = 'block'
+        point.element.style.left = `${translateX}px`
+        point.element.style.top = `${translateY}px`
+      } else {
+        point.element.style.display = 'none'
+      }
+    }
+
     public getPosPoints(){
       return this.posPoints
+    }
+
+    private pointPositioning(e){
+      // On convertit la position de la souris dans le repère de la caméra 
+      let mouse = new THREE.Vector2(
+        (e.clientX / window.innerWidth) * 2 - 1,
+        -(e.clientY / window.innerHeight) * 2 + 1
+      )
+      this.rayCaster.setFromCamera(mouse, this.camera)
+      let intersects = this.rayCaster.intersectObject(this.sphere)
+      if (intersects.length > 0) {
+        console.log('Sphère touchée au point : ', intersects[0].point)
+      }
     }
 }
--- main.ts
+++ main.ts
@@ -1,9 +1,25 @@
 import './style.css'
+import * as THREE from 'three'
 import ImageFocusPoints from './ImageFocusPoints'
 import HandArrow from './HandArrow'
+import PanoramaFocusPoints from './PanoramaFocusPoints'
 
-const imgFocus = new ImageFocusPoints(document.querySelector('canvas'), './arte.jpg', [255, 8, 164], [document.querySelector('.Pointer#p1'),document.querySelector('.Pointer#p2')])
+// const imgFocus = new ImageFocusPoints(document.querySelector('canvas'), './arte.jpg', [255, 8, 164], [document.querySelector('.Pointer#p1'),document.querySelector('.Pointer#p2')])
 
-setTimeout(()=>{
-    new HandArrow(imgFocus.getPosPoints()[0], 'coucou <br> ceci est un <br> test', 30)
-}, 100)
+// setTimeout(()=>{
+//     new HandArrow(imgFocus.getPosPoints()[0], 'coucou <br> ceci est un <br> test', 30)
+// }, 100)
+
+new PanoramaFocusPoints(
+    document.querySelector('canvas'),
+    './360.jpg',
+    [
+        {
+            element : document.querySelector('.Pointer#p1'),
+            position: new THREE.Vector3( 0.5317732054597113, 5.1725616166319055, 499.6663880423187 )
+        },{
+            element : document.querySelector('.Pointer#p2'),
+            position: new THREE.Vector3( 361.56787937319723, -31.973578131749214, -342.79565611937784 )
+        }
+    ]
+)
